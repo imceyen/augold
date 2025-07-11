168a06ef19f01f5014eaac76ffc259313a932e45
musictocktock <sjhoon0573@gmail.com>
2025-07-11 12:37:24 +0900

    로그인 성공

--- src/main/java/com/global/augold/mainPage/controller/MainPageController.java
+++ src/main/java/com/global/augold/mainPage/controller/MainPageController.java
@@ -1,15 +1,15 @@
 package com.global.augold.mainPage.controller;
 
-import com.global.augold.goldPrice.dto.GoldPriceDTO;
-// import com.global.augold.goldPrice.Service.GoldPriceService;
 import com.global.augold.mainPage.dto.MainPageInfoDTO;
 import com.global.augold.mainPage.service.MainPageService;
+import com.global.augold.member.entity.Customer;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -17,18 +17,18 @@ import java.util.List;
 public class MainPageController {
 
     private final MainPageService mainPageService;
-    //private final GoldPriceService goldPriceService;
 
     @GetMapping("/")
-    public String showMainPage(Model model) {
+    public String showMainPage(Model model, HttpSession session) {
         List<MainPageInfoDTO> products = mainPageService.getMainPageProducts();
-        //GoldPriceDTO todayPrice = goldPriceService.getTodayGoldPrice();
-        //GoldPriceDTO[] historyPrices = goldPriceService.getGoldPriceHistory();
-
         model.addAttribute("products", products);
-        //model.addAttribute("todayPrice", todayPrice);
-        //model.addAttribute("historyPrices", Arrays.asList(historyPrices));
 
-        return "main"; // templates/main.html
+        Customer loginCustomer = (Customer) session.getAttribute("loginUser");
+        if (loginCustomer != null) {
+            model.addAttribute("loginName", loginCustomer.getCstmName());
+        }
+
+        return "main";
     }
+
 }
