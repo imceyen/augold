6ab246f5ea9cff21d570d9bb984cb803e5c56182
musictocktock <sjhoon0573@gmail.com>
2025-07-14 16:34:45 +0900

    문의내역 CRUD

--- src/main/java/com/global/augold/member/repository/CSInquiryRepository.java
+++ src/main/java/com/global/augold/member/repository/CSInquiryRepository.java
@@ -5,9 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CSInquiryRepository extends JpaRepository<CSInquiry, String> {
 
-    @Query("SELECT MAX(c.inqNumber) FROM CSInquiry c WHERE c.inqNumber LIKE :todayPrefix")
+    @Query("SELECT MAX(c.inqNumber) FROM CSInquiry c WHERE c.inqNumber LIKE CONCAT(:todayPrefix, '%')")
     String findMaxInquiryNumberForToday(@Param("todayPrefix") String todayPrefix);
+
+    List<CSInquiry> findByCstmNumberOrderByInqDateDesc(String cstmNumber);
+
+    List<CSInquiry> findByCstmNumberAndInqTitleContainingIgnoreCaseOrderByInqDateDesc(String cstmNumber, String keyword);
 
 }
